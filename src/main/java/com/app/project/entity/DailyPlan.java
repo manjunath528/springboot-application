649902415156304ffc8e0aa09eb72c996403b814
manjunath528 <bmanjunath505@gmail.com>
2024-07-02 20:41:22 +0200

    Added Entity and Repo class for the DailyPlan

---
+++ src/main/java/com/app/project/entity/DailyPlan.java
@@ -0,0 +1,117 @@
+package com.app.project.entity;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "daily_plan")
+public class DailyPlan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_sequence_daily_plan")
+    @SequenceGenerator(name = "id_sequence_daily_plan", sequenceName = "sequence_daily_plan", allocationSize = 1)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "login_id", unique = true)
+    private String loginId;
+
+    @Column(name = "calories_goal")
+    private Long caloriesGoal;
+
+    @Column(name = "protein_goal")
+    private Long proteinGoal;
+
+    @Column(name = "carbs_goal")
+    private Long carbsGoal;
+
+    @Column(name = "fats_goal")
+    private Long fatsGoal;
+
+    @Column(name = "created_ts")
+    private Date createdTs;
+
+    @Column(name = "updated_ts")
+    private Date updatedTs;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public Long getCaloriesGoal() {
+        return caloriesGoal;
+    }
+
+    public void setCaloriesGoal(Long caloriesGoal) {
+        this.caloriesGoal = caloriesGoal;
+    }
+
+    public Long getProteinGoal() {
+        return proteinGoal;
+    }
+
+    public void setProteinGoal(Long proteinGoal) {
+        this.proteinGoal = proteinGoal;
+    }
+
+    public Long getCarbsGoal() {
+        return carbsGoal;
+    }
+
+    public void setCarbsGoal(Long carbsGoal) {
+        this.carbsGoal = carbsGoal;
+    }
+
+    public Long getFatsGoal() {
+        return fatsGoal;
+    }
+
+    public void setFatsGoal(Long fatsGoal) {
+        this.fatsGoal = fatsGoal;
+    }
+
+    public Date getCreatedTs() {
+        return createdTs;
+    }
+
+    public void setCreatedTs(Date createdTs) {
+        this.createdTs = createdTs;
+    }
+
+    public Date getUpdatedTs() {
+        return updatedTs;
+    }
+
+    public void setUpdatedTs(Date updatedTs) {
+        this.updatedTs = updatedTs;
+    }
+
+    @Override
+    public String toString() {
+        return "DailyPlan{" +
+                "id=" + id +
+                ", loginId='" + loginId + '\'' +
+                ", caloriesGoal=" + caloriesGoal +
+                ", proteinGoal=" + proteinGoal +
+                ", carbsGoal=" + carbsGoal +
+                ", fatsGoal=" + fatsGoal +
+                ", createdTs=" + createdTs +
+                ", updatedTs=" + updatedTs +
+                '}';
+    }
+
+}
+
